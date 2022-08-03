c1ad56a2e3920724ca26386c8da6480b97f68e11
Alexander Täschner <AlexanderTaeschner@googlemail.com>
2022-08-03 18:52:44 +0200

    Add method for using UTF8 string literals.

--- SQLiteLibrary/NativeMethods.cs
+++ SQLiteLibrary/NativeMethods.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace SQLiteLibrary;
 
-internal static partial class NativeMethods
+internal static class NativeMethods
 {
     /// <summary>
     /// The SQLITE_OK result code means that the operation was successful and that there were no errors. Most other result codes indicate an error.
@@ -119,93 +119,93 @@ internal static partial class NativeMethods
     internal static string FromUtf8(IntPtr ptr)
         => Marshal.PtrToStringUTF8(ptr) ?? throw new ArgumentNullException(nameof(ptr));
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static unsafe partial int sqlite3_open_v2(byte* utf8Filename, out SQLiteConnectionHandle connectionHandle, int flags, IntPtr vfsModule);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static unsafe extern int sqlite3_open_v2(byte* utf8Filename, out SQLiteConnectionHandle connectionHandle, int flags, IntPtr vfsModule);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_close(IntPtr connectionHandle);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_close(IntPtr connectionHandle);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_busy_timeout(SQLiteConnectionHandle connectionHandle, int timeoutInMilliSeconds);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_busy_timeout(SQLiteConnectionHandle connectionHandle, int timeoutInMilliSeconds);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_extended_result_codes(SQLiteConnectionHandle connectionHandle, int onoff);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_extended_result_codes(SQLiteConnectionHandle connectionHandle, int onoff);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial long sqlite3_last_insert_rowid(SQLiteConnectionHandle connectionHandle);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern long sqlite3_last_insert_rowid(SQLiteConnectionHandle connectionHandle);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial IntPtr sqlite3_errmsg(SQLiteConnectionHandle connectionHandle);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern IntPtr sqlite3_errmsg(SQLiteConnectionHandle connectionHandle);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial IntPtr sqlite3_errstr(int resultCode);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern IntPtr sqlite3_errstr(int resultCode);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static unsafe partial int sqlite3_prepare_v2(SQLiteConnectionHandle connectionHandle, byte* utf8SQLStatement, int utf8SQLStatementByteLength, out SQLiteStatementHandle statementHandle, out byte* tail);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static unsafe extern int sqlite3_prepare_v2(SQLiteConnectionHandle connectionHandle, byte* utf8SQLStatement, int utf8SQLStatementByteLength, out SQLiteStatementHandle statementHandle, out byte* tail);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_finalize(IntPtr statementHandle);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_finalize(IntPtr statementHandle);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_bind_blob(SQLiteStatementHandle statementHandle, int index, byte[] value, int valueByteLength, IntPtr destructor);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_bind_blob(SQLiteStatementHandle statementHandle, int index, byte[] value, int valueByteLength, IntPtr destructor);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_bind_double(SQLiteStatementHandle statementHandle, int index, double value);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_bind_double(SQLiteStatementHandle statementHandle, int index, double value);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_bind_int(SQLiteStatementHandle statementHandle, int index, int value);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_bind_int(SQLiteStatementHandle statementHandle, int index, int value);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_bind_int64(SQLiteStatementHandle statementHandle, int index, long value);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_bind_int64(SQLiteStatementHandle statementHandle, int index, long value);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_bind_null(SQLiteStatementHandle statementHandle, int index);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_bind_null(SQLiteStatementHandle statementHandle, int index);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static unsafe partial int sqlite3_bind_text(SQLiteStatementHandle statementHandle, int index, byte* utf8Text, int utf8TextByteLength, IntPtr destructor);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static unsafe extern int sqlite3_bind_text(SQLiteStatementHandle statementHandle, int index, byte* utf8Text, int utf8TextByteLength, IntPtr destructor);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static unsafe partial int sqlite3_bind_parameter_index(SQLiteStatementHandle statementHandle, byte* utf8ParameterName);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static unsafe extern int sqlite3_bind_parameter_index(SQLiteStatementHandle statementHandle, byte* utf8ParameterName);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_bind_zeroblob(SQLiteStatementHandle statementHandle, int index, int blobLength);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_bind_zeroblob(SQLiteStatementHandle statementHandle, int index, int blobLength);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_bind_zeroblob64(SQLiteStatementHandle statementHandle, int index, long blobLength);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_bind_zeroblob64(SQLiteStatementHandle statementHandle, int index, long blobLength);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_step(SQLiteStatementHandle statementHandle);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_step(SQLiteStatementHandle statementHandle);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_reset(SQLiteStatementHandle statementHandle);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_reset(SQLiteStatementHandle statementHandle);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial IntPtr sqlite3_column_blob(SQLiteStatementHandle statementHandle, int columnIndex);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern IntPtr sqlite3_column_blob(SQLiteStatementHandle statementHandle, int columnIndex);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial double sqlite3_column_double(SQLiteStatementHandle statementHandle, int columnIndex);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern double sqlite3_column_double(SQLiteStatementHandle statementHandle, int columnIndex);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_column_int(SQLiteStatementHandle statementHandle, int columnIndex);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_column_int(SQLiteStatementHandle statementHandle, int columnIndex);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial long sqlite3_column_int64(SQLiteStatementHandle statementHandle, int columnIndex);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern long sqlite3_column_int64(SQLiteStatementHandle statementHandle, int columnIndex);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial IntPtr sqlite3_column_text(SQLiteStatementHandle statementHandle, int columnIndex);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern IntPtr sqlite3_column_text(SQLiteStatementHandle statementHandle, int columnIndex);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_column_bytes(SQLiteStatementHandle statementHandle, int columnIndex);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_column_bytes(SQLiteStatementHandle statementHandle, int columnIndex);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_column_type(SQLiteStatementHandle statementHandle, int columnIndex);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_column_type(SQLiteStatementHandle statementHandle, int columnIndex);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial int sqlite3_column_count(SQLiteStatementHandle statementHandle);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern int sqlite3_column_count(SQLiteStatementHandle statementHandle);
 
-    [LibraryImport(SQLiteLibraryFileName)]
-    internal static partial IntPtr sqlite3_column_name(SQLiteStatementHandle statementHandle, int columnIndex);
+    [DllImport(SQLiteLibraryFileName)]
+    internal static extern IntPtr sqlite3_column_name(SQLiteStatementHandle statementHandle, int columnIndex);
 
-    [LibraryImport("kernel32.dll", StringMarshalling = StringMarshalling.Utf16)]
-    private static partial IntPtr LoadLibrary(string dllToLoad);
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+    private static extern IntPtr LoadLibrary(string dllToLoad);
 }
