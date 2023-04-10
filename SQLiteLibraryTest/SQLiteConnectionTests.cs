ea54a11b7625c306dce246920cf495501ea949f8
Alexander Täschner <AlexanderTaeschner@googlemail.com>
2023-04-10 18:36:51 +0200

    Update native dll version to 3.41.2.

--- SQLiteLibraryTest/SQLiteConnectionTests.cs
+++ SQLiteLibraryTest/SQLiteConnectionTests.cs
@@ -112,7 +112,7 @@ public class SQLiteConnectionTests
     {
         using var conn = SQLiteConnection.CreateTemporaryInMemoryDb();
         string value = conn.ExecuteScalarStringQuery("SELECT sqlite_version();"u8);
-        Assert.Equal("3.41.0", value);
+        Assert.Equal("3.41.2", value);
     }
 
     [Fact]
