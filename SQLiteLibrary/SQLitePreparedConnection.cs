56897b1fbb1557e29ad109f68b00ac5cb5f95936
Alexander Täschner <AlexanderTaeschner@googlemail.com>
2024-01-15 18:33:58 +0100

    Add new type to represent a SQLite connection with prepared statements.

---
+++ SQLiteLibrary/SQLitePreparedConnection.cs
@@ -0,0 +1,92 @@
+﻿// <copyright file="SQLitePreparedConnection.cs" company="Alexander Täschner">
+// Copyright (c) Alexander Täschner. All rights reserved.
+// </copyright>
+
+namespace SQLiteLibrary;
+
+/// <summary>
+/// Provides a sqlite connection with prepared SQL statements.
+/// </summary>
+/// <typeparam name="TEnum">Enum type used as key to the SQL texts.</typeparam>
+public sealed class SQLitePreparedConnection<TEnum> : IDisposable
+    where TEnum : Enum
+{
+    private readonly Dictionary<TEnum, SQLiteStatement> _preparedStatements;
+    private readonly SQLiteConnection _connection;
+    private bool _disposed;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SQLitePreparedConnection{TEnum}"/> class.
+    /// </summary>
+    /// <param name="sqlTextDictionary">Dictionary with SQL texts.</param>
+    /// <param name="createSQLiteConnection">Function used to create a new SQLiteConnection instance.</param>
+    public SQLitePreparedConnection(IReadOnlyDictionary<TEnum, string> sqlTextDictionary, Func<SQLiteConnection> createSQLiteConnection)
+    {
+        _connection = createSQLiteConnection();
+        var preparedStatements = new Dictionary<TEnum, SQLiteStatement>();
+        foreach (KeyValuePair<TEnum, string> kvp in sqlTextDictionary)
+        {
+#pragma warning disable DNSQLL001 // Type or member is obsolete
+            SQLiteStatement stmt = _connection.PrepareStatement(kvp.Value);
+#pragma warning restore DNSQLL001 // Type or member is obsolete
+            preparedStatements.Add(kvp.Key, stmt);
+        }
+
+        _preparedStatements = preparedStatements;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SQLitePreparedConnection{TEnum}"/> class.
+    /// </summary>
+    /// <param name="sqlTextDictionary">Dictionary with SQL texts.</param>
+    /// <param name="createSQLiteConnection">Function used to create a new SQLiteConnection instance.</param>
+    public SQLitePreparedConnection(IReadOnlyDictionary<TEnum, byte[]> sqlTextDictionary, Func<SQLiteConnection> createSQLiteConnection)
+    {
+        _connection = createSQLiteConnection();
+        var preparedStatements = new Dictionary<TEnum, SQLiteStatement>();
+        foreach (KeyValuePair<TEnum, byte[]> kvp in sqlTextDictionary)
+        {
+            SQLiteStatement stmt = _connection.PrepareStatement(kvp.Value);
+            preparedStatements.Add(kvp.Key, stmt);
+        }
+
+        _preparedStatements = preparedStatements;
+    }
+
+    /// <summary>
+    /// Get the prepared statement.
+    /// </summary>
+    /// <param name="sqlTextKey">Key to the SQL text.</param>
+    /// <returns>The prepared staement.</returns>
+    public SQLiteStatement GetStatement(TEnum sqlTextKey) => _preparedStatements[sqlTextKey];
+
+    /// <summary>
+    /// Execute a prepared non query SQL statement.
+    /// </summary>
+    /// <param name="sqlTextKey">Key to the SQL text.</param>
+    public void ExecuteNonQuery(TEnum sqlTextKey) => _preparedStatements[sqlTextKey].DoneStep();
+
+    /// <summary>
+    /// Return the prepared statement.
+    /// </summary>
+    /// <param name="preparedStatement">A prepared statement.</param>
+    public void Return(SQLiteStatement preparedStatement) => preparedStatement.Reset();
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        foreach (SQLiteStatement stmt in _preparedStatements.Values)
+        {
+            stmt.Dispose();
+        }
+
+        _preparedStatements.Clear();
+        _connection.Dispose();
+        _disposed = true;
+    }
+}
---
+++ SQLiteLibraryTest/SQLitePreparedConnectionTests.cs
@@ -0,0 +1,106 @@
+﻿// <copyright file="SQLitePreparedConnectionTests.cs" company="Alexander Täschner">
+// Copyright (c) Alexander Täschner. All rights reserved.
+// </copyright>
+
+using SQLiteLibrary;
+using Xunit;
+
+namespace SQLiteLibraryTest;
+
+public class SQLitePreparedConnectionTests
+{
+    private const string FileName = "SQLitePreparedConnectionTests_TestDb.sqlite";
+    private int _openDatabaseCalls = 0;
+
+    private enum SQLitePreparedConnectionTestStatements
+    {
+        InsertValue,
+        InsertValue43,
+        SelectValue,
+    }
+
+    [Fact]
+    public void SQLitePreparedConnection_Works()
+    {
+        if (File.Exists(FileName))
+        {
+            File.Delete(FileName);
+        }
+
+        using (var conn = SQLiteConnection.CreateNewOrOpenExistingDb(FileName))
+        {
+            conn.ExecuteNonQuery("CREATE TABLE t(x INTEGER);"u8);
+        }
+
+        Assert.True(File.Exists(FileName));
+
+        IReadOnlyDictionary<SQLitePreparedConnectionTestStatements, string> sqlTextDictionary = new Dictionary<SQLitePreparedConnectionTestStatements, string>()
+        {
+            { SQLitePreparedConnectionTestStatements.InsertValue, "INSERT INTO t VALUES (@value);" },
+            { SQLitePreparedConnectionTestStatements.InsertValue43, "INSERT INTO t VALUES (43);" },
+            { SQLitePreparedConnectionTestStatements.SelectValue, "SELECT x FROM t;" },
+        };
+
+        using var preparedConnection = new SQLitePreparedConnection<SQLitePreparedConnectionTestStatements>(sqlTextDictionary, CreateSQLiteConnection);
+        TestPreparedConnection(preparedConnection);
+    }
+
+    [Fact]
+    public void SQLitePreparedConnection_Works2()
+    {
+        if (File.Exists(FileName))
+        {
+            File.Delete(FileName);
+        }
+
+        using (var conn = SQLiteConnection.CreateNewOrOpenExistingDb(FileName))
+        {
+            conn.ExecuteNonQuery("CREATE TABLE t(x INTEGER);"u8);
+        }
+
+        Assert.True(File.Exists(FileName));
+
+        IReadOnlyDictionary<SQLitePreparedConnectionTestStatements, byte[]> sqlTextDictionary = new Dictionary<SQLitePreparedConnectionTestStatements, byte[]>()
+        {
+            { SQLitePreparedConnectionTestStatements.InsertValue, "INSERT INTO t VALUES (@value);"u8.ToArray() },
+            { SQLitePreparedConnectionTestStatements.InsertValue43, "INSERT INTO t VALUES (43);"u8.ToArray() },
+            { SQLitePreparedConnectionTestStatements.SelectValue, "SELECT x FROM t;"u8.ToArray() },
+        };
+
+        using var preparedConnection = new SQLitePreparedConnection<SQLitePreparedConnectionTestStatements>(sqlTextDictionary, CreateSQLiteConnection);
+        TestPreparedConnection(preparedConnection);
+    }
+
+    private void TestPreparedConnection(SQLitePreparedConnection<SQLitePreparedConnectionTestStatements> preparedConnection)
+    {
+        int i;
+        for (i = 1; i <= 42; i++)
+        {
+            SQLiteStatement insertStmt = preparedConnection.GetStatement(SQLitePreparedConnectionTestStatements.InsertValue);
+            insertStmt.BindParameter("@value"u8, i);
+            insertStmt.DoneStep();
+            preparedConnection.Return(insertStmt);
+        }
+
+        preparedConnection.ExecuteNonQuery(SQLitePreparedConnectionTestStatements.InsertValue43);
+
+        i = 1;
+        SQLiteStatement selectStmt = preparedConnection.GetStatement(SQLitePreparedConnectionTestStatements.SelectValue);
+        while (selectStmt.TryNewRowStep())
+        {
+            int value = selectStmt.GetColumnIntegerValue(0);
+            Assert.Equal(i++, value);
+        }
+
+        Assert.Equal(44, i);
+        preparedConnection.Return(selectStmt);
+
+        Assert.Equal(1, _openDatabaseCalls);
+    }
+
+    private SQLiteConnection CreateSQLiteConnection()
+    {
+        _openDatabaseCalls++;
+        return SQLiteConnection.OpenExistingDb(FileName);
+    }
+}
