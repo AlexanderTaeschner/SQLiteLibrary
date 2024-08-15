77fcaafabe0d20660a74eefb5056374083e7ccbf
Alexander Täschner <AlexanderTaeschner@googlemail.com>
2024-08-15 20:23:33 +0200

    Native SQLite version 3.46.1.

--- SQLiteLibraryTest/SQLiteConnectionTests.cs
+++ SQLiteLibraryTest/SQLiteConnectionTests.cs
@@ -126,7 +126,7 @@ public class SQLiteConnectionTests
     {
         using var conn = SQLiteConnection.CreateTemporaryInMemoryDb();
         string value = conn.ExecuteScalarStringQuery("SELECT sqlite_version();"u8);
-        Assert.Equal("3.46.0", value);
+        Assert.Equal("3.46.1", value);
     }
 
     [Fact]
